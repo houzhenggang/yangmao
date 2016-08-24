018ab76f17fd927175e3834902cce83bc52c2b8d
xuximing <xuxim99@hotmail.com>
2016-08-24 17:19:51 +0800

    db changes

--- yangmao-doc/yangmao.sql
+++ yangmao-doc/yangmao.sql
@@ -80,8 +80,8 @@ CREATE TABLE `yangmao_favorites_item` (
   `original_price` decimal(7,2) COMMENT '原价，单位元',
   `final_price` decimal(7,2) COMMENT '折后价，单位元',
   `final_price_wap` decimal(7,2) COMMENT '无线折扣价，即宝贝在无线上的实际售卖价格',
-  `click_url` varchar(300) COMMENT '淘客地址',
-  `tk_rate` decimal(7,2) COMMENT '淘客百分率,39.0表示39%',
+  `click_url` varchar(2000) COMMENT '淘客地址',
+  `tk_rate` decimal(7,2) COMMENT '淘客百分率,39.0表示39.0%',
   `volume` int(11) COMMENT '30天销量',   
   `create_time` datetime comment '创建时间',
   `last_update_time` datetime comment '最后一次更新时间',
@@ -90,4 +90,46 @@ CREATE TABLE `yangmao_favorites_item` (
   KEY `yangmao_favorites_id` (`yangmao_favorites_id`),
   KEY `title` (`title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='选品表';
+
+#邮件模板表
+CREATE TABLE `yangmao_mail_template` (
+  `template_id` bigint NOT NULL  COMMENT '邮件模板ID',
+  `name` varchar(50) COMMENT '模板名称，变量{}',
+  `title` varchar(100) COMMENT '模板标题,变量{}',
+  `content` text COMMENT '模板内容',
+  `section` varchar(50) COMMENT '区段名称',
+  `section_amount` int COMMENT '区段可容纳得商品数量',
+  `status` tinyint COMMENT '0：可使用，1:已删除',
+  `create_time` datetime comment '创建时间',
+  `last_update_time` datetime comment '最后一次更新时间',
+  PRIMARY KEY (`template_id`),
+  KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='邮件模板表';
+
+#邮件实例表
+CREATE TABLE `yangmao_mail_instance` (
+  `mail_instance_id` bigint NOT NULL  COMMENT '邮件实例ID',
+  `template_id` bigint COMMENT '邮件模板ID',
+  `title` varchar(50) COMMENT '模板名称，变量{}',
+  `content` text COMMENT '邮件内容，变量{}',
+  `status` tinyint COMMENT '0：可使用，1:已下架',
+  `expire_time` datetime comment '到期时间',
+  `create_time` datetime comment '创建时间',
+  `last_update_time` datetime comment '最后一次更新时间',
+  PRIMARY KEY (`mail_instance_id`),
+   KEY (`template_id`),
+   KEY (`expire_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='邮件实例表';
+
+#邮件实例选品表
+CREATE TABLE `yangmao_mail_instance_item` (
+  `instance_item_id` bigint NOT NULL  COMMENT '邮件实例中的商品ID',
+  `mail_instance_id` bigint NOT NULL  COMMENT '邮件实例ID',
+  `item_id` bigint COMMENT '选品ID',
+  `section` varchar(50) COMMENT '隶属的区段名称',
+  PRIMARY KEY (`instance_item_id`),
+  KEY (`mail_instance_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='邮件实例的选品表';
+
+
 
