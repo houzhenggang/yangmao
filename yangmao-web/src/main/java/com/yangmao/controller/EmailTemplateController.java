ade40a1f6bdb15b43c3e23a8d7b3857d41128f5d
liyongfeng <15301392163@163.com>
2016-08-24 18:58:38 +0800

    email 模板控制器

--- yangmao-web/src/main/java/com/yangmao/controller/EmailTemplateController.java
+++ yangmao-web/src/main/java/com/yangmao/controller/EmailTemplateController.java
@@ -12,5 +12,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(RouteKey.ADMIN)
 public class EmailTemplateController {
 
+    /**
+     * 添加模板
+     */
+    @RequestMapping(RouteKey.ADD_EMAIL_TEMPLATE)
+    public void addEmailTemplate(){
+
+    }
+
+    /**
+     * 保存模板
+     * @return
+     */
+    @RequestMapping(RouteKey.INSERT_EMAIL_TEMPLATE)
+    public String insertEmailTemplate(){
+
+        return "redirect:email_template_list.html";
+    }
+
+    @RequestMapping(RouteKey.EMAIL_TEMPLATE_LIST)
+    public void emailTemplateList(){
+
+    }
+
+    @RequestMapping(RouteKey.MODIFY_EMAIL_TEMPLATE)
+    public void modifyEmailTemplate(){
+
+    }
+
+    @RequestMapping(RouteKey.UPDATE_EMAIL_TEMPLATE)
+    public String updateEmailTemplate(){
+        return "redirect:email_template_list.html";
+    }
+
+    @RequestMapping(RouteKey.DELETE_EMAIL_TEMPALTE)
+    public String deleteEmailTemplate(){
+        return "redirect:email_template_list.html";
+    }
 
 }
